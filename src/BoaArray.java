8e4ef9e90403d5d6ff0ae14082d7cca86a658140
Md Johirul Islam <johirbuet@gmail.com>
2017-06-27 16:51:55 -0500

    Boa Array Converter

--- src/BoaArray.java
+++ src/BoaArray.java
@@ -1,4 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class BoaArray {
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner sc =new Scanner(new File("kmeans.txt"));
+		StringBuilder sb =new StringBuilder();
+		sb.append("a: array of int = {");
+		while(sc.hasNextLine()){
+			String gid = sc.nextLine();
+			sb.append(gid);
+			if(sc.hasNextLine()){
+				sb.append(",");
+			}
+		}
+		sb.append("};");
+		System.out.println(sb.toString());
+		sc.close();
+	}
 }
